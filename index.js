eb1f153c3c73b989dadbefec53a5cd0b31baccb6
Kim-Robin <robin1265@gmail.com>
2020-03-31 17:40:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//jshint esversion:6
+
+let gamePattern =[];
+let buttons = ["yellow", "blue", "black", "red", "green", "purple"];
+let userChosenPattern = [];
+
+let gameStart = false;
+let level = 0;
